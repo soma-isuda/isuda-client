5c07e50f4ac3bb85a77b8f8308a2f3d0ebb2d32c
choids <dh_stz@naver.com>
2014-09-28 23:57:59 +0900

    편성표 카테고리 포커스 처리중

--- Framework/app/javascript/TVSchedulePg.js
+++ Framework/app/javascript/TVSchedulePg.js
@@ -1,3 +1,7 @@
+//global variables
+var TVSchedulePg_index = 0;
+var big_index = 0;
+var mid_index = 0;
 
 var TVSchedulePg= {
 
@@ -10,7 +14,8 @@ TVSchedulePg.onLoad = function()
 	//document.getElementById("TVSchedulePg").style.marginLeft="1460px";
 	jQuery.extend(TVSchedulePg,{
 		big : jQuery('#big').find('ul'),
-		mid : jQuery('#mid').find('ul'),
+	    mid : jQuery('#mid').find('ul'),
+        
 		anchor: {
 			big : jQuery('#anchor_TVSchedulePg_bigCategory'),
 			mid : jQuery('#anchor_TVSchedulePg_midCategory'),
@@ -23,19 +28,23 @@ TVSchedulePg.onLoad = function()
 	    TVSchedulePg.big.append('<li>' + firstCategory[i] + '</li>');
 	}
 	this.focus();
+
+    //CSS를 위한 선택자
 	jQuery.extend(TVSchedulePg,{
-	    bigElem : jQuery('#big').find('ul > li')
+	    bigElem: jQuery('#big').find('ul > li'),
+	    midElem:jQuery('#mid').find('ul>li')
 	});
+    //첫번째 대분류 카테고리에 초점을 맞춘상태로 시작한다.
+	TVSchedulePg.bigElem.eq(big_index).addClass('focus');
 };
 
-var TVSchedulePg_index =0;
-var big_index=0;
-var mid_index=0;
+
 TVSchedulePg.focus = function(){ 
 	alert("");
 	alert("TVSchedulePg.focus");
 	TVSchedulePg.anchor.big.focus();
-	TVSchedulePg.big.eq(big_index).addClass('focus');
+
+    
 	tabMenu();
 	// focus initialize
 	//TVSchedulePg_index =0;
@@ -52,14 +61,15 @@ tabMenu = function(){
 
 	alert("tabMenu");
     //clear mid tag
-	jQuery('.mid').empty();
+	jQuery('#mid').find('ul').empty();
     // Load second category
-	for (var i = 1; i < secondCategory[big_index].length ; i++) 
+	for (var i = 1; i < secondCategory[big_index].length ; i++) {
 	    TVSchedulePg.mid.append('<li>' + secondCategory[big_index][i] + '</li>');
-	    
+	}
 	
 };
 
+//대분류에서의 키처리를 담당하는 부분
 TVSchedulePg.bigKeyDown = function()
 {
 	alert("TVSchedulePg big Category keyDown");
@@ -75,31 +85,46 @@ TVSchedulePg.bigKeyDown = function()
 			alert("TVSchedulePg_key : RETURN");
 			SelectWatchPg.onLoad();
 			break;
-		case tvKey.KEY_LEFT:
+	    case tvKey.KEY_LEFT:
 			alert("TVSchedulePg_key : Left");
 			break;
-		case tvKey.KEY_RIGHT:
-			alert("TVSchedulePg_key : Right");
+	    case tvKey.KEY_RIGHT:
+	        //현재 보고 있던 대분류의 포커스는 유지한 상태로
+	        //중분류 첫번째에 포커스를 준뒤,
+	        //TVSchedulePg.midKeyDown으로 키처리를 넘긴다.
+	        alert("TVSchedulePg_key : Right");
+
+            //중분류로 포커스를 넘긴다.
+	        TVSchedulePg.anchor.mid.focus();
+	        //중분류 첫번째에 초점을 맞춘상태로 시작한다.
+	        TVSchedulePg.midElem.eq(mid_index).addClass('focus');
 			break;
 		case tvKey.KEY_UP:
 		    alert("TVSchedulePg_key : Up");
-            //중분류 카테고리의 맨위에 도달했을때 위의 키를 누르면 , 맨아래로 간다.
+            
+		    TVSchedulePg.bigElem.eq(big_index).removeClass('focus');
+		    //중분류 카테고리의 맨위에 도달했을때 위의 키를 누르면 , 맨아래로 간다.
 		    if (big_index == 0)
-		        big_index = firstCategory.length - 1;
+		        big_index = firstCategory.length-1;
+		    else
+		        big_index--;
 
-			TVSchedulePg.bigElem.eq(big_index).removeClass('focus');
-			TVSchedulePg.bigElem.eq(--big_index).addClass('focus');
+			TVSchedulePg.bigElem.eq(big_index).addClass('focus');
 			tabMenu();
 			
 			break;
 		case tvKey.KEY_DOWN:
 		    alert("TVSchedulePg_key : Down");
-            //중분류 카테고리의 맨 아래에 도달했을때 아래 키를 누르면, 맨위로 간다.
-		    if (big_index == firstCategory.length - 1) {
+            
+		  
+		    TVSchedulePg.bigElem.eq(big_index).removeClass('focus');
+		    //중분류 카테고리의 맨 아래에 도달했을때 아래 키를 누르면, 맨위로 간다.
+		    if (big_index == firstCategory.length-1)
 		        big_index = 0;
-		    }
-			TVSchedulePg.bigElem.eq(big_index).removeClass('focus');
-			TVSchedulePg.bigElem.eq(++big_index).addClass('focus');
+		    else
+		        big_index++;
+
+			TVSchedulePg.bigElem.eq(big_index).addClass('focus');
 			tabMenu();
 			
 			break;
@@ -108,7 +133,7 @@ TVSchedulePg.bigKeyDown = function()
 			alert("TVSchedulePg_key : Enter");
 			TVSchedulePg.big.eq(big_index).removeClass('focus');
 			tabMenu();
-			TVSchedulePg.anchor.mid.focus();
+			TVSchedulePg.anchor.mid.focus();//중분류로 anchor를 넘긴다
 			TVSchedulePg.mid.eq(mid_index).find('li').focus();
 			break;
 		default:
